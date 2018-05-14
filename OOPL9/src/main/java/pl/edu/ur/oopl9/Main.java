1b11f9e0c6fc496755f0d7fd4f8c7a0aa5eec0e6
ACER <ACER@192.168.1.102>
2018-05-14 22:10:24 +0200

    Filip Szpunar

--- OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
+++ OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
 import java.util.TreeSet;
+import java.util.HashSet;
+import java.util.Scanner;
 /**
  *
  * @author maarchyl
@@ -20,6 +22,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         // TODO code application logic here
         //zad.1
         int[] tab = new int[100];
@@ -69,6 +72,63 @@ public class Main {
             }
         
     }
- 
+        //zad.4
+        HashSet<String> słowa = new HashSet<>();
+        Scanner odczyt = new Scanner(System.in);
+
+        int f = 0;
+        while (f == 0) {
+            System.out.println("podaj słowo po polsku do przetłumaczenia spośród: ");
+            System.out.println("'i', 'w' ,'nie','na','z','do','tak','on','dobranoc',drzwi','ale','dlaczego','jeden','przepraszam','co','dom','kiedy','ona','nigdy','wszystko'");
+            System.out.println("Jeśli chcesz zakończyć wpisz 'koniec'");
+            String d = odczyt.nextLine();
+            słowa.add(d);
+            if (słowa.contains("i")) {
+                System.out.println("and");
+            } else if (słowa.contains("w")) {
+                System.out.println("in");
+            } else if (słowa.contains("nie")) {
+                System.out.println("no");
+            } else if (słowa.contains("na")) {
+                System.out.println("on");
+            } else if (słowa.contains("z")) {
+                System.out.println("from");
+            } else if (słowa.contains("do")) {
+                System.out.println("to");
+            } else if (słowa.contains("tak")) {
+                System.out.println("yes");
+            } else if (słowa.contains("on")) {
+                System.out.println("he");
+            } else if (słowa.contains("dobranoc")) {
+                System.out.println("good night");
+            } else if (słowa.contains("drzwi")) {
+                System.out.println("door");
+            } else if (słowa.contains("ale")) {
+                System.out.println("but");
+            } else if (słowa.contains("dlaczego")) {
+                System.out.println("why");
+            } else if (słowa.contains("jeden")) {
+                System.out.println("one");
+            } else if (słowa.contains("przepraszam")) {
+                System.out.println("I'm sorry");
+            } else if (słowa.contains("co")) {
+                System.out.println("what");
+            } else if (słowa.contains("dom")) {
+                System.out.println("house");
+            } else if (słowa.contains("kiedy")) {
+                System.out.println("when");
+            } else if (słowa.contains("ona")) {
+                System.out.println("she");
+            } else if (słowa.contains("nigdy")) {
+                System.out.println("never");
+            } else if (słowa.contains("wszystko")) {
+                System.out.println("everything");
+            } else if (słowa.contains("koniec")) {
+                f = 1;
+            }
+            słowa.remove(d);
+
+        }
+
     }
 }
